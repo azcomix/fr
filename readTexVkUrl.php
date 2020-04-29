6cd25eec4617467a48286a28f2376688fa17e7a0
azcomix <64530544+azcomix@users.noreply.github.com>
2020-04-29 13:10:14 +0200

    Update readTexVkUrl.php

--- readTexVkUrl.php
+++ readTexVkUrl.php
@@ -4,7 +4,7 @@
 echo "Tex Willer (French)<hr>";
 // Get a file into an array.  In this example we'll go through HTTP to get
 // the HTML source of a URL.
-$lines = file('Tex-pdf.txt');
+$lines = file('Tex-pdf.fr.txt');
 
 // Loop through our array, show HTML source as HTML source; and line numbers too.
 foreach ($lines as $line_num => $line) {
