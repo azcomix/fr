e605a6466b0288bf364d1df0422c94418cc7b6b5
azcomix <64530544+azcomix@users.noreply.github.com>
2020-07-03 11:43:35 +0200

    Create Fourre-Tout-01.js

---
+++ showfromurl/Fourre-Tout-01.js
@@ -0,0 +1,15 @@
+//Fourre-Tout-01.js
+
+var url=[
+"http://2.bp.blogspot.com/--n4d0s6m4ek/VA2DeHKhaUI/AAAAAAAAKGk/iLekeprJW9U/s1600/2FOURRE-TOUT-1.jpg",
+"http://3.bp.blogspot.com/-rTkkdJ8EguI/VA2DifJgoOI/AAAAAAAAKGw/cd2d4AgzmvU/s1600/3FOURRE-TOUT-2.jpg",
+"http://1.bp.blogspot.com/-1YuAkUs_5yI/VA2DjVAGOSI/AAAAAAAAKG8/t7IL_roCRoA/s1600/4FOURRE-TOUT-3.jpg",
+"http://1.bp.blogspot.com/-XuSB4fRAeHU/VA2DiaN54LI/AAAAAAAAKG0/Syv4BvzQ87o/s1600/5FOURRE-TOUT-4.jpg",
+"http://1.bp.blogspot.com/-HS4MBJQeuwg/VA2DnnxWpaI/AAAAAAAAKHQ/E392He4SScc/s1600/6FOURRE-TOUT-5.jpg",
+"http://1.bp.blogspot.com/-2auNY1kIFR8/VA2DmL9FURI/AAAAAAAAKHI/ZG-5DlCoCa0/s1600/7FOURRE-TOUT-6.jpg",
+"http://2.bp.blogspot.com/-qGNpxQPbAgg/VA2DomAFhBI/AAAAAAAAKHY/fUuM8PqmKuc/s1600/8FOURRE-TOUT-7.jpg",
+"http://2.bp.blogspot.com/-2n5HmoZB7W8/VA2DqGas61I/AAAAAAAAKHg/ZbUagIa1URY/s1600/9FOURRE-TOUT-8.jpg",
+"http://3.bp.blogspot.com/-pK5jSQl31VY/VA2DaRJs3wI/AAAAAAAAKGM/Lg405CuEH_A/s1600/10FOURRE-TOUT-9.jpg",
+"http://1.bp.blogspot.com/-WO4QoNK4Hxs/VA2Db2T4TsI/AAAAAAAAKGY/0lfRk5UiTUU/s1600/11FOURRE-TOUT-10.jpg",
+"http://3.bp.blogspot.com/-OzYFMQZJGBk/VA2DaLUH66I/AAAAAAAAKGI/QTPM17ajQC0/s1600/12FOURRE-TOUT-11.jpg"];
+
